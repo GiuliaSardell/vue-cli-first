1737304dfbb61762e798da86a1eb0f32b1593df6
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-11-23 16:24:24 +0100

    title aggiunto come componente

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-
+  <Title />
 </template>
 
 <script>
-
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Title
   }
 }
 </script>
 
 <style>
-
+  body{
+    text-align: center;
+  }
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <h1>Questo è un titolo</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<style scoped>
+h1{
+  color: rebeccapurple;
+}
+
+</style>
