e4e68116442909946d5d9bbdf99ae13c8ac98066
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-11-23 17:26:55 +0100

    modifica header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
   <header>
     <h1>La mia prima app con Vue CLI!</h1>
+    <h3>(Questo è l'header) </h3>
   </header>
 </template>
 
@@ -11,10 +12,15 @@ export default {
 </script>
 
 <style scoped>
-h1{
+h1,h3{
   color: blueviolet; 
   padding: 50px 0;
 }
+
+h3{
+  padding-top: 0;
+}
+
 header{
   background-color: pink;
 
